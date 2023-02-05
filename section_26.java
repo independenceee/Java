c710bf9d470760665bb04162662dbef6012f433b
Independence <nguyenkhanh17112003@gmail.com>
2023-02-05 19:48:08 +0700

    section 28

--- section_26.java
+++ section_26.java
@@ -2,8 +2,30 @@ public class Main {
 
     public static void main(String[] args) {
         
-        
+        Car myCar = new Car();
+
+        System.out.println(myCar.make);
+        System.out.println(myCar.model);
+        System.out.println();
+        myCar.driver();
+        myCar.brake();
 
     }
 
+}
+
+public class Car {
+    String name = "mercedes";
+    String model = "2019";
+    int year = 2019;
+    String color = "blue";
+    double price = 50000.00;
+
+    void driver() {
+        System.out.println("Driver in charge");
+    }
+
+    void brake() {
+        System.out.println("Bravo in charge");
+    }
 }
---
+++ section_27.java
@@ -0,0 +1,38 @@
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Human human = new Human("Khanh", 20, 70);
+        Human human2 = new Human("Independence", 20, 68);
+        System.out.println(human.name);
+
+        human2.eat();
+        human1.drink();
+
+    }
+}
+
+public class Human {
+
+   String name;
+   int age;
+   double weight;
+
+    Human(String name, int age, double weight) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+    }
+
+    void eat() {
+        System.out.println(this.name + ": " + this.age);
+    }
+
+    void drink() {
+        System.out.println(this.name + " drinks");
+    }
+
+}
