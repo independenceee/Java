80cfbf92a8d8d98640d731168eee7ed2c0f7271f
Independence <nguyenkhanh17112003@gmail.com>
2023-02-12 16:47:41 +0700

    java

---
+++ section_38.java
@@ -0,0 +1,11 @@
+// public: full
+// private: class
+// protected: class package subclass
+// no-modifier: class package
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
